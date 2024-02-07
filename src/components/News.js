1f67cc90e2e0b7e3cc57a360604551dfdcf7424f
Samz0001 <sishere23@gmail.com>
2024-02-07 20:32:23 +0530

    Added API TO ENV

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
+
 import NewsItem from "./NewsItem";
-import Spinner from "./Spinner."; // Corrected import statement
+import Spinner from "./Spinner.";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
@@ -15,19 +16,17 @@ const News = (props) => {
   };
 
   const updateNews = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${
-      props.country
-    }&category=${props.category}&apiKey=464e21b80f9e4dfbbea551a7f285e9d4${
-      props.apiKey ? "&" + props.apiKey : ""
-    }&page=${page}&pageSize=${props.pageSize}`;
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true);
     let data = await fetch(url);
-
+    props.setProgress(30);
     let parsedData = await data.json();
-
+    props.setProgress(70);
     setArticles(parsedData.articles);
     setTotalResults(parsedData.totalResults);
     setLoading(false);
+    props.setProgress(100);
   };
 
   useEffect(() => {
@@ -39,13 +38,13 @@ const News = (props) => {
   const fetchMoreData = async () => {
     const url = `https://newsapi.org/v2/top-headlines?country=${
       props.country
-    }&category=${props.category}&apiKey=464e21b80f9e4dfbbea551a7f285e9d4${
-      props.apiKey ? "&" + props.apiKey : ""
-    }&page=${page + 1}&pageSize=${props.pageSize}`;
+    }&category=${props.category}&apiKey=${props.apiKey}&page=${
+      page + 1
+    }&pageSize=${props.pageSize}`;
     setPage(page + 1);
     let data = await fetch(url);
     let parsedData = await data.json();
-    setArticles((prevArticles) => prevArticles.concat(parsedData.articles));
+    setArticles(articles.concat(parsedData.articles));
     setTotalResults(parsedData.totalResults);
   };
 
@@ -66,12 +65,9 @@ const News = (props) => {
       >
         <div className="container">
           <div className="row">
-            {articles.map((element, index) => {
-              // Adding index parameter for unique keys
+            {articles.map((element) => {
               return (
-                <div className="col-md-4" key={`${element.url}_${index}`}>
-                  {" "}
-                  {/* Combining URL with index for unique keys */}
+                <div className="col-md-4" key={element.url}>
                   <NewsItem
                     title={element.title ? element.title : ""}
                     description={element.description ? element.description : ""}
