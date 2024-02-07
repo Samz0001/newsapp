8e9d4655c26f15b294eaaefc1833efba130e63d6
Samz0001 <sishere23@gmail.com>
2024-02-07 21:35:25 +0530

    final

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,55 +1,52 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absolute",
-              right: "0",
-            }}
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          {" "}
+          <span className=" badge rounded-pill bg-danger">{source}</span>
+        </div>
+
+        <img
+          src={
+            !imageUrl
+              ? "https://www.livemint.com/lm-img/img/2024/02/03/1600x900/Small_cap_stock_under_Rs_100_stock_market_news_1706956902851_1706956903003.jpg"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {!author ? "unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn btn-dark"
           >
-            {" "}
-            <span className=" badge rounded-pill bg-danger">{source}</span>
-          </div>
-
-          <img
-            src={
-              !imageUrl
-                ? "https://www.livemint.com/lm-img/img/2024/02/03/1600x900/Small_cap_stock_under_Rs_100_stock_market_news_1706956902851_1706956903003.jpg"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {!author ? "unknown" : author} on{" "}
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a
-              rel="noreferrer"
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn btn-dark"
-            >
-              Read More
-            </a>
-          </div>
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
